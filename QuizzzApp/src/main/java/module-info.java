3598096ccf5406779f4e93c39aabf9605c1dfa7a
ImranImon14 <imraniimon14@gmail.com>
2025-07-04 02:32:06 +0600

    Delete QuizzzApp directory

--- QuizzzApp/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.quizzzapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.quizzzapp to javafx.fxml;
-    exports com.example.quizzzapp;
-}
