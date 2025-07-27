5babfaef0fedaa8507e13dd8c7a881dab987a19f
ImranImon14 <imraniimon14@gmail.com>
2025-07-27 14:28:50 +0600

    Delete Final_Project directory

--- Final_Project/src/main/java/utils/OTPUtil.java
+++
@@ -1,8 +0,0 @@
-package main.utils;
-
-public class OTPUtil {
-    public static String generateOtp() {
-        int otp = (int)(Math.random() * 9000) + 1000; // 4-digit random OTP
-        return String.valueOf(otp);
-    }
-}
